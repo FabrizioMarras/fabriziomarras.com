6554db88632a91bec03ddd1901c22c3976c6af63
fabrizio marras <fmarras79@gmail.com>
2023-11-29 01:42:16 +0100

    fix the THREE.BufferGeometry error in console.

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,8 @@ import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2})
+  // IMPORTANT: the Float32Array value needs to be a number divisible by 3 to avoid an error in the console.
+  const sphere = random.inSphere(new Float32Array(6000), { radius: 1.2})
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
@@ -13,7 +14,7 @@ const Stars = (props) => {
 
   return (
     <group rotation={[ 0, 0, Math.PI / 4 ]}>
-      <Points ref={ref} positions={sphere} stride={3} frustomCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
