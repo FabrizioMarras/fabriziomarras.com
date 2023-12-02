12ffb13748883462667c38537f7d758c53a0a731
fabrizio marras <fmarras79@gmail.com>
2023-12-02 12:54:57 +0100

    updated theme colors in comnfig file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+        primary: "#ec1d24", // Red
+        secondary: "#f6921f", //Orange
+        tertiary: "#2dc4b6", // water
+        text: "#dfd9ff",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
