cac5238b24d5b2e98aa50f3ba9175d0132f33989
fabrizio marras <fmarras79@gmail.com>
2023-12-02 13:22:55 +0100

    updated styles and theme

--- src/styles.js
+++ src/styles.js
@@ -4,12 +4,12 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
     heroHeadText:
-      "font-black text-white lg:text-[70px] sm:text-[46px] xs:text-[36px] text-[30px] lg:leading-[80px] mt-2",
+      "font-medium text-white lg:text-[70px] sm:text-[46px] xs:text-[36px] text-[30px] lg:leading-[80px] mt-2",
     heroSubText:
-      "font-light lg:text-[20px] sm:text-[18px] xs:text-[16px] text-[14px] lg:leading-[30px] tracking-wider",
+      "font-light lg:text-[32px] sm:text-[24px] xs:text-[18px] text-[16px] lg:leading-[50px] tracking-wider",
   
     sectionHeadText:
-      "text-secondary font-black md:text-[60px] sm:text-[40px] xs:text-[30px] text-[30px]",
+      "text-primary font-medium md:text-[60px] sm:text-[40px] xs:text-[30px] text-[30px]",
     sectionSubText:
       "font-light sm:text-[18px] text-[14px] text-tertiary uppercase tracking-wider",
 
