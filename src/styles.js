e697758a05cf59402a4ef1f3ed62c7b2a17e76e9
fabrizio marras <fmarras79@gmail.com>
2023-12-02 12:55:13 +0100

    updated fonts styles

--- src/styles.js
+++ src/styles.js
@@ -6,12 +6,15 @@ const styles = {
     heroHeadText:
       "font-black text-white lg:text-[70px] sm:text-[46px] xs:text-[36px] text-[30px] lg:leading-[80px] mt-2",
     heroSubText:
-      "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[18px] xs:text-[16px] text-[14px] lg:leading-[30px]",
+      "font-light lg:text-[20px] sm:text-[18px] xs:text-[16px] text-[14px] lg:leading-[30px] tracking-wider",
   
     sectionHeadText:
-      "text-white font-black md:text-[60px] sm:text-[40px] xs:text-[30px] text-[30px]",
+      "text-secondary font-black md:text-[60px] sm:text-[40px] xs:text-[30px] text-[30px]",
     sectionSubText:
-      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+      "font-light sm:text-[18px] text-[14px] text-tertiary uppercase tracking-wider",
+
+    pText: 
+    "font-light sm:text-[16px] text-[13px] text-white tracking-wider"
   };
   
   export { styles };
