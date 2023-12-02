18c29ebf9d442596482ff6058303e271ea5e7688
fabrizio marras <fmarras79@gmail.com>
2023-12-02 18:21:54 +0100

    added heroImage

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import Hero from "./Hero.png";
+import Hero_hover from "./Hero_hover.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -59,4 +60,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  Hero,
+  Hero_hover
 };
