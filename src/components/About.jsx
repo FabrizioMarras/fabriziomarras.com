1e7adf927cd43f888b9010939146784a7c940360
fabrizio marras <fmarras79@gmail.com>
2023-12-05 18:59:56 +0100

    updated motion animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const ServiceCard = ({ index, title, icon, points }) => {
       className="sm:max-w-[350px] w-full"
     >
       <motion.div
-        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        variants={fadeIn("right", "spring", 0.3 * index, 0.45)}
         className="w-full tertiary-primary-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
@@ -62,10 +62,13 @@ const About = () => {
         ))}
       </div>
       <motion.div
-        variants={textVariant()}
-        className="mt-20">
-        <p className={`${styles.sectionSubText} mb-8`}>More about myself</p>
-        <p>With a Chemistry degree from Italy, I pursued a Ph.D. in Amsterdam, later contributing six years to impactful research in the chemical industry. Driven by a desire for new challenges, I explored photography and graphic design before transitioning into IT and web development, discovering a profound passion for programming. As a frontend developer, I identified the need for improved organizational efficiency, leading me to step into the role of a project manager. Subsequently, I embraced the position of a product owner in a Marketing Automation company, expanding my skills into marketing. Currently, I'm engaged in AI-driven projects, reflecting my commitment to adaptability and continuous learning at the forefront of technological trends. My journey highlights a diverse skill set and a passion for driving innovative solutions.</p>
+        variants={textVariant(2)}
+        className="mt-20 max-w-3xl">
+       <motion.p 
+        variants={fadeIn("", "", 2, 1)}
+        className={`${styles.sectionSubText} mb-8`}>More about myself</motion.p>
+      <motion.p 
+        variants={fadeIn("", "", 2.5, 1)}>With a Chemistry degree from Italy, I pursued a Ph.D. in Amsterdam, later contributing six years to impactful research in the chemical industry. Driven by a desire for new challenges, I explored photography and graphic design before transitioning into IT and web development, discovering a profound passion for programming. As a frontend developer, I identified the need for improved organizational efficiency, leading me to step into the role of a project manager. Subsequently, I embraced the position of a product owner in a Marketing Automation company, expanding my skills into marketing. Currently, I'm engaged in AI-driven projects, reflecting my commitment to adaptability and continuous learning at the forefront of technological trends. My journey highlights a diverse skill set and a passion for driving innovative solutions.</motion.p>
       </motion.div>
     </>
   )
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,19 @@
 import { CubeCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
 
 
 const Tech = () => {
   return (
     <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6 gap-5">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
+      {technologies.map((technology, index) => (
+        <motion.div 
+          variants={fadeIn("right", "spring", 0.2 * index, 0.25)}
+          className="w-28 h-28" key={technology.name}>
           <CubeCanvas icon={technology.icon} />
-        </div>
+        </motion.div>
       ))}
     </div> 
   )
