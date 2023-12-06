f816138b2d74480370e3888ad57f567352017132
fabrizio marras <fmarras79@gmail.com>
2023-12-06 16:36:40 +0100

    created dynamic Project Pages

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,14 +1,29 @@
+// ProjectPage.jsx
 import { useParams } from 'react-router-dom';
-import Navbar from '../components/Navbar'; 
+import Navbar from '../components/Navbar';
+import { projects } from '../constants';
 
-const ProjectPage = ({ params }) => {
-  const {projectId} = useParams();
+const ProjectPage = () => {
+  const { projectId } = useParams();
+  const project = projects.find((p) => p.id === projectId);
+
+  if (!project) {
+    // Handle case where project is not found
+    return <div>Project not found</div>;
+  }
+
+  const heroImageStyle = {
+    backgroundImage: `url(${project.image})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+  };
 
   return (
     <div>
-      <div className="bg-product-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className="w-full h-screen" style={heroImageStyle}>
         <Navbar isHomePage={false} />
-        <h1>Product {projectId}</h1>
+        <h1>{project.name}</h1>
       </div>
     </div>
   );
