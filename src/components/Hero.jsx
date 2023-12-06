3ba43f0075762a040808222efdac88c73218eb79
fabrizio marras <fmarras79@gmail.com>
2023-12-06 16:35:29 +0100

    moved down the scroll icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         </div>
       </div>
         <ComputersCanvas />
-      <div className="absolute bottom-10 xs:bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[20px] h-[44px] rounded-3xl border-2 border-gray-200 flex justify-center items-start p-1">
             <motion.div 
