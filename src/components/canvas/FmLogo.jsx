284dddaee86da2c96b451811bae7032291c8c7cd
fabrizio marras <fmarras79@gmail.com>
2023-12-08 18:14:20 +0100

    customisation of dimension per each cubical object.

--- src/components/canvas/FmLogo.jsx
+++ src/components/canvas/FmLogo.jsx
@@ -6,25 +6,37 @@ import CanvasLoader from '../Loader';
 const AnimatedParallelepipeds = () => {
   const group = useRef();
 
-  // Define state variables to control rotation for each parallelepiped
-  const [rotations, setRotations] = useState(Array.from({ length: 5 }, () => ({ x: 0, y: 0 })));
+  // Define state variable to control properties for each parallelepiped
+  const [parallelepipeds, setParallelepipeds] = useState([
+    { position: [-3, 0, 0], dimensions: [1, 2, 3], rotation: [0, 0, 0] },
+    { position: [-1, 0, 0], dimensions: [1.5, 2, 2], rotation: [0, 0, 0] },
+    { position: [1, 0, 0], dimensions: [2, 1, 3], rotation: [0, 0, 0] },
+    { position: [3, 0, 0], dimensions: [1, 3, 2], rotation: [0, 0, 0] },
+    { position: [5, 0, 0], dimensions: [2, 2, 2], rotation: [0, 0, 0] },
+    { position: [7, 0, 0], dimensions: [1, 1, 4], rotation: [0, 0, 0] },
+  ]);
 
   // Use the useFrame hook to animate each parallelepiped independently
   useFrame(() => {
-    // Update the rotations based on the current state
-    setRotations(prevRotations =>
-      prevRotations.map(rotation => ({
-        x: rotation.x + 0.01,
-        y: rotation.y + 0.01,
+    // Update the rotation for each parallelepiped
+    setParallelepipeds(prevParallelepipeds =>
+      prevParallelepipeds.map(parallelepiped => ({
+        ...parallelepiped,
+        rotation: [parallelepiped.rotation[0] + 0.01, parallelepiped.rotation[1] + 0.01, 0],
       }))
     );
   });
 
   return (
     <group ref={group}>
-      {/* Create multiple parallelepipeds in a grid */}
-      {rotations.map((rotation, index) => (
-        <Box key={index} position={[(index - 2) * 2, 0, 0]} args={[1, 2, 3]} rotation={[rotation.x, rotation.y, 0]}>
+      {/* Create parallelepipeds based on the state array */}
+      {parallelepipeds.map((parallelepiped, index) => (
+        <Box
+          key={index}
+          position={parallelepiped.position}
+          args={parallelepiped.dimensions}
+          rotation={parallelepiped.rotation}
+        >
           {/* Set the dimensions and rotation of each parallelepiped */}
           {/* You can customize the appearance of each parallelepiped here */}
           <meshStandardMaterial color={'#00ff00'} />
@@ -36,7 +48,7 @@ const AnimatedParallelepipeds = () => {
 
 const FmLogo = () => {
   return (
-    <div className="w-full h-screen">
+    <div>
       FmLogo
       <Canvas>
         <Suspense fallback={<CanvasLoader />}>
