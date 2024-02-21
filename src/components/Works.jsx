f9121f3f7d78fbb5c99541a36d06ad4db72effc6
fabrizio marras <fmarras79@gmail.com>
2024-02-21 23:33:06 +0100

    remove Tilt from project card on mobile

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -63,40 +63,63 @@ const Filter = ({ handleFilterChange, filters }) => {
   )
 }
 
-const ProjectCard = ({ index, name, description, techs, image }) => {
+const ProjectCard = ({ index, name, description, techs, image, isMobile }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}
     initial="hidden"
   animate="show"
   exit="exit">
-        <Tilt
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
-          className="bg-gray-800 p-5 border-2 border-tertiary rounded-2xl w-full h-full flex flex-col justify-between">
-          <div>
-          <div className="relative w-full h-[230px]">
-            <img
-              src={image}
-              alt={`project-${name}`}
-              className="w-full h-full object-cover rounded-2xl"
-            />
-          </div>
-          <div className="mt-5">
-            <h3 className="text-tertiary font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-white text-[14px]">{description}</p>
-          </div>
-          </div>
-          <div>
-             <div className="mt-8 flex flex-row justify-end flex-wrap gap-2">
-              {techs.map((tech) => (
-                <img key={tech.name} className={`w-[24px] h-[24px]`} src={tech.img} alt={tech.name} />
-              ))}
-            </div>
-          </div>
-        </Tilt>
+    {isMobile ? (
+      <div className="bg-gray-800 p-5 border-2 border-tertiary rounded-2xl w-full h-full flex flex-col justify-between">
+      <div>
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={`project-${name}`}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+      </div>
+      <div className="mt-5">
+        <h3 className="text-tertiary font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-white text-[14px]">{description}</p>
+      </div>
+      </div>
+      <div>
+         <div className="mt-8 flex flex-row justify-end flex-wrap gap-2">
+          {techs.map((tech) => (
+            <img key={tech.name} className={`w-[24px] h-[24px]`} src={tech.img} alt={tech.name} />
+          ))}
+        </div>
+      </div>
+    </div>
+    ) : (<Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-gray-800 p-5 border-2 border-tertiary rounded-2xl w-full h-full flex flex-col justify-between">
+      <div>
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={`project-${name}`}
+          className="w-full h-full object-cover rounded-2xl"
+        />
+      </div>
+      <div className="mt-5">
+        <h3 className="text-tertiary font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-white text-[14px]">{description}</p>
+      </div>
+      </div>
+      <div>
+         <div className="mt-8 flex flex-row justify-end flex-wrap gap-2">
+          {techs.map((tech) => (
+            <img key={tech.name} className={`w-[24px] h-[24px]`} src={tech.img} alt={tech.name} />
+          ))}
+        </div>
+      </div>
+    </Tilt>)}
     </motion.div>
 )}
 
@@ -160,10 +183,10 @@ const Works = () => {
             className={`${styles.pText} mt-3`}>
             Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problem, work with different technologies, and manage project effectively.
           </motion.p>
-          <motion.div variants={textVariant()}
+          <div variants={textVariant()}
             className="mt-10 px-1 relative z-10 flex justify-end">
                 <Filter handleFilterChange={handleFilterChange} filters={filters} />
-            </motion.div>
+            </div>
         </motion.div>
         ) : (
           <div>
