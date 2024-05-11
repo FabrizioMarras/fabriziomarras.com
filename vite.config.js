b25540ba6f727460896746d40c9eea6283654b77
fabrizio marras <fmarras79@gmail.com>
2024-05-11 22:10:40 +0200

    fix apiurl endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ command }) => {
+  const isProduction = command === 'build';
+
+  // Define environment variable based on the environment
+  const apiUrl = isProduction
+    ? 'https://server.fabriziomarras.com'
+    : 'http://localhost:3333';
+
+  return defineConfig({
+    plugins: [
+      react(),
+    ],
+    define: {
+      'process.env': {
+        VITE_REACT_APP_API_ENDPOINT: JSON.stringify(apiUrl),
+      },
+    },
+  });
+};
